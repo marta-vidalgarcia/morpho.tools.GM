46b0bd4ffe224cc510ba2fc932a482f1b7d2110b
Marta Vidal-Garcia <marta.vidalga@gmail.com>
2020-04-15 20:31:53 -0600

    Update. Can import a single *tag file

---
+++ R/tag2array.R
@@ -0,0 +1,63 @@
+#' @name tag2array
+#' @title tag2array
+#' 
+#' @description
+#'   This function inputs *tag files into an array of dimensions p, k, n
+#'   
+#' @usage
+#'   tag2array(dir = NULL, ID = NULL, string_del = NULL)
+#'   
+#' @param dir Optional argument. Directory where the *.ascii files are. Default (NULL) is the current working directory.
+#' 
+#' @param ID Optional argument for specimens IDs. The default (NULL) is the file names.
+#' 
+#' @param string_del Optional argument. Indicates pattern in the specimens names to be deleted. Use only if ID = NULL.
+#' 
+#' @return This function returns an array of dimensions (p, k, n), in which p is the number of landmarks, k = 3 (number of dimensions), and n is the number of specimens.
+#'
+#' @examples
+#' # array <- tag2array()
+#' # If we have other *.tag files in another directory and with the suffix "skull"
+#' # For example: "spec1_skull.tag", "spec2_skull.tag", "spec3_skull.tag", etc.
+#' # dir <- "~/Documents/skull_LMs")
+#' # skull_array <- tag2array(dir = skull_dir, string_del="skull")
+#'   
+#' @author Marta Vidal-Garcia
+#' @export
+#' 
+
+tag2array <- function(dir = NULL, ID = NULL, string_del = NULL){
+  if (is.null(dir) == TRUE){
+    path <- getwd()
+  }
+  else{
+    path <- dir
+  }
+  tag_list <- dir(path = path, pattern="*.tag")
+  n_land <- vector("numeric", length=length(tag_list))
+  for (i in 1:length(tag_list)){
+    n_land[i] <- length(count.fields(tag_list[[i]])) -3
+  }
+  if (is.null(ID) == TRUE){
+    if (is.null(string_del) == TRUE){
+      dimnames_tag <- gsub(".tag", "", tag_list)
+    }
+    else{
+      dimnames_tag <- gsub(string_del, "", gsub(".tag", "", tag_list))
+    }
+  }
+  else{
+    dimnames_tag <- ID
+  }
+  if (length(unique(n_land))!=1){
+    stop("Specimens have different number of landmarks.")
+  }
+  LM_array <- array(data = NA, dim = c(n_land[1], 3, length(tag_list)))
+  dimnames(LM_array)[[3]] <- dimnames_tag
+  for (i in 1:length(tag_list)){
+    LM_array[,1,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 4, sep = " ", header=F))[, 2]
+    LM_array[,2,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 4, sep = " ", header=F))[, 3]
+    LM_array[,3,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 4, sep = " ", header=F))[, 4]
+  }
+  return(LM_array)
+}
--- R/tag2lm.R
+++ R/tag2lm.R
@@ -2,62 +2,23 @@
 #' @title tag2lm
 #' 
 #' @description
-#'   This function inputs *tag files into an array of dimensions p, k, n
+#'   This function inputs a single *tag file into an matrix with the landmark coordinates (x, y, z)
 #'   
 #' @usage
-#'   tag2lm(dir = NULL, ID = NULL, string_del = NULL)
+#'   tag2lm(file)
 #'   
-#' @param dir Optional argument. Directory where the *.ascii files are. Default (NULL) is the current working directory.
+#' @param file MINC *.tag file to import.
 #' 
-#' @param ID Optional argument for specimens IDs. The default (NULL) is the file names.
+#' @return This function returns matrix with the landmark coordinates (x, y, z)
 #' 
-#' @param string_del Optional argument. Indicates pattern in the specimens names to be deleted. Use only if ID = NULL.
-#' 
-#' @return This function returns an array of dimensions (p, k, n), in which p is the number of landmarks, k = 3 (number of dimensions), and n is the number of specimens.
-#'
 #' @examples
-#' # array <- tag2lm()
-#' # If we have *.tag file in another directory and with the suffix "skull"
-#' # For example: "spec1_skull.tag", "spec2_skull.tag", "spec3_skull.tag", etc.
-#' # dir <- "~/Documents/skull_LMs")
-#' # skull_array <- tag2lm(dir = skull_dir, string_del="skull")
+#' # specimen1 <- tag2lm("specimen1.tag")
 #'   
 #' @author Marta Vidal-Garcia
 #' @export
 #' 
 
-tag2lm <- function(dir = NULL, ID = NULL, string_del = NULL){
-  if (is.null(dir) == TRUE){
-    path <- getwd()
-  }
-  else{
-    path <- dir
-  }
-  tag_list <- dir(path = path, pattern="*.tag")
-  n_land <- vector("numeric", length=length(tag_list))
-  for (i in 1:length(tag_list)){
-    n_land[i] <- length(count.fields(tag_list[[i]])) -3
-  }
-  if (is.null(ID) == TRUE){
-    if (is.null(string_del) == TRUE){
-      dimnames_tag <- gsub(".tag", "", tag_list)
-      }
-    else{
-      dimnames_tag <- gsub(string_del, "", gsub(".tag", "", tag_list))
-      }
-    }
-  else{
-  dimnames_tag <- ID
-  }
-  if (length(unique(n_land))!=1){
-    stop("Specimens have different number of landmarks.")
-  }
-  LM_array <- array(data = NA, dim = c(n_land[1], 3, length(tag_list)))
-  dimnames(LM_array)[[3]] <- dimnames_tag
-  for (i in 1:length(tag_list)){
-    LM_array[,1,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 4, sep = " ", header=F))[, 2]
-    LM_array[,2,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 4, sep = " ", header=F))[, 3]
-    LM_array[,3,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 4, sep = " ", header=F))[, 4]
-  }
-  return(LM_array)
+tag2lm <- function(file){
+  lm_matrix <- suppressWarnings(read.table(file = file, skip = 4, sep = " ", header=F))[, 2:4]
+  return(lm_matrix)
 }
