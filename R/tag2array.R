00f4f1e99e1d526fd1c441923855184c55d8a509
Marta Vidal-Garcia <marta.vidalga@gmail.com>
2020-05-12 16:50:04 -0600

    added minor changes to documentation and added 2 options in tag2array

--- R/tag2array.R
+++ R/tag2array.R
@@ -5,13 +5,17 @@
 #'   This function inputs *tag files into an array of dimensions p, k, n
 #'   
 #' @usage
-#'   tag2array(dir = NULL, ID = NULL, string_del = NULL)
+#'   tag2array(dir = NULL, ID = NULL, string_del = NULL, propagated = FALSE, save.txt = FALSE)
 #'   
 #' @param dir Optional argument. Directory where the *.ascii files are. Default (NULL) is the current working directory.
 #' 
 #' @param ID Optional argument for specimens IDs. The default (NULL) is the file names.
 #' 
 #' @param string_del Optional argument. Indicates pattern in the specimens names to be deleted. Use only if ID = NULL.
+#' 
+#' @param propagated Optional argument indicating if the landmarks have been propagated (extra line). The default is FALSE.
+#' 
+#' @param save.txt Optional argument. Indicates whether to also save *.txt files with the landmark coordinates.
 #' 
 #' @return This function returns an array of dimensions (p, k, n), in which p is the number of landmarks, k = 3 (number of dimensions), and n is the number of specimens.
 #'
@@ -26,7 +30,7 @@
 #' @export
 #' 
 
-tag2array <- function(dir = NULL, ID = NULL, string_del = NULL){
+tag2array <- function(dir = NULL, ID = NULL, string_del = NULL, propagated = FALSE, save.txt = FALSE){
   if (is.null(dir) == TRUE){
     path <- getwd()
   }
@@ -36,7 +40,12 @@ tag2array <- function(dir = NULL, ID = NULL, string_del = NULL){
   tag_list <- dir(path = path, pattern="*.tag")
   n_land <- vector("numeric", length=length(tag_list))
   for (i in 1:length(tag_list)){
-    n_land[i] <- length(count.fields(tag_list[[i]])) -3
+    if (isTRUE(propagated) == TRUE){
+      n_land[i] <- length(count.fields(tag_list[[i]])) -4
+    }
+    else {
+      n_land[i] <- length(count.fields(tag_list[[i]])) -3
+    }
   }
   if (is.null(ID) == TRUE){
     if (is.null(string_del) == TRUE){
@@ -54,10 +63,24 @@ tag2array <- function(dir = NULL, ID = NULL, string_del = NULL){
   }
   LM_array <- array(data = NA, dim = c(n_land[1], 3, length(tag_list)))
   dimnames(LM_array)[[3]] <- dimnames_tag
-  for (i in 1:length(tag_list)){
-    LM_array[,1,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 4, sep = " ", header=F))[, 2]
-    LM_array[,2,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 4, sep = " ", header=F))[, 3]
-    LM_array[,3,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 4, sep = " ", header=F))[, 4]
+  if (isTRUE(propagated) == TRUE){
+    for (i in 1:length(tag_list)){
+      LM_array[,1,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 5, sep = " ", header=F))[, 2]
+      LM_array[,2,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 5, sep = " ", header=F))[, 3]
+      LM_array[,3,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 5, sep = " ", header=F))[, 4]
+    }
+  }
+  else{
+    for (i in 1:length(tag_list)){
+      LM_array[,1,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 4, sep = " ", header=F))[, 2]
+      LM_array[,2,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 4, sep = " ", header=F))[, 3]
+      LM_array[,3,i] <- suppressWarnings(read.table(file = tag_list[[i]], skip = 4, sep = " ", header=F))[, 4]
+    }
+  }
+  if (isTRUE(save.txt) == TRUE){
+    for (i in 1:dim(LM_array)[3]){
+      write.table(LM_array[,,i], paste0(dimnames(LM_array)[[3]][i], ".txt"))
+    }
   }
   return(LM_array)
 }
